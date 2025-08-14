3a6a98f590817a58ca15872f14a96e6b2f70a5f8
Gustavo2059 <gustavomai2059@gmail.com>
2025-08-14 19:58:17 -0300

    add exercise.py

---
+++ python_tests/exercise.py
@@ -0,0 +1,6 @@
+#Create a Python function that takes an integer ( n ) as input and generates a dictionary containing pairs
+#( (i, i^2) ) for all integers ( i ) from 1 to ( n ) (inclusive). The function should then return this dictionary.
+
+def foda:
+    n = int(input('Digite um número inteiro: '))
+    for i in range (n):
